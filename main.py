a71ee0a7ad84c86788f0d624098e9f98b8efda93
NElabyad <noor.elabyad@analog.com>
2026-03-25 20:21:59 +0200

    M3 : CLI working in main.py

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import argparse
 import os
 from src.data_prep.normalizer import Normalizer
 from src.model.ngram_model import NGramModel 
+from src.inference.predictor import Predictor
 load_dotenv(dotenv_path="config/.env")
 
 def main():
 
     parser = argparse.ArgumentParser(description="run upto which step")
-    parser.add_argument("--step", default="dataprep", help="run upto which step")
+    parser.add_argument("--step", default="inference", help="run upto which step")
     args = parser.parse_args()
 
     if args.step == "dataprep":
-        normalizer1 = Normalizer(folder_path=os.getenv("TRAIN_RAW_DIR"), output_file=os.getenv("TRAIN_TOKENS"))
 
-        # Training
-        normalizer1.main()
+        normalizer = Normalizer(
+            folder_path=os.getenv("TRAIN_RAW_DIR"), 
+            output_file=os.getenv("TRAIN_TOKENS")
+        )
 
-        # Normalizer Test
-        print(Normalizer.normalize("  Hello, World! This is a test. 123   "))
+        normalizer.main()
+
 
     if args.step == "model":
-        ngram_model1 = NGramModel (
+
+        ngram_model = NGramModel (
             token_file=os.getenv("TRAIN_TOKENS"),
             model_path=os.getenv("MODEL_PATH"),
             vocab_path=os.getenv("VOCAB_PATH"),
@@ -29,12 +32,39 @@ def main():
             ngram_order=int(os.getenv("NGRAM_ORDER"))
         )
 
-        # Trainig 
-        ngram_model1.main()
+        ngram_model.main()
 
-        # Test lookup
-        print(ngram_model1.lookup("hi across the"))
-        print(ngram_model1.lookup("the"))
+    if args.step == "inference":
+
+        normalizer=Normalizer(
+            folder_path=os.getenv("TRAIN_RAW_DIR"), 
+            output_file=os.getenv("TRAIN_TOKENS")
+            )
+        ngram_model=NGramModel(
+            token_file=os.getenv("TRAIN_TOKENS"),
+            model_path=os.getenv("MODEL_PATH"),
+            vocab_path=os.getenv("VOCAB_PATH"),
+            unk_threshold=int(os.getenv("UNK_THRESHOLD")),
+            ngram_order=int(os.getenv("NGRAM_ORDER"))
+            )
+        ngram_model.load()
+
+        predictor = Predictor(
+            ngram_model=ngram_model,
+            normalizer=normalizer,
+            top_k=int(os.getenv("TOP_K"))
+            )
+        
+        
+        while True:
+            user_input = input("Type sentence (or type 'quit' to exit): ")
+
+            if user_input.lower() == "quit":
+                break
+
+            print(predictor.predict_next(user_input))
+
+    
     
 
 if __name__ == "__main__":
